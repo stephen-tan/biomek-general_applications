7f19e34fc3a72b9be7af5d2ee65756850487ee1e
Stephen Tan <82525229+stephen-tan@users.noreply.github.com>
2021-07-20 12:22:44 -0700

    Add files via upload

---
+++ generateCSV.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    function WriteToCSV() {
+        // https://stackoverflow.com/questions/17564103/using-javascript-to-download-file-as-a-csv-file
+        // filename in format "GeneralApplications_MethodName_MM-DD-YYYY-mm-hh-ss"
+
+        // Set variable for datetime
+
+
+        // Loop through all elements in the General Applications interface
+        // each variable name will be the parameter "name" and its value is the element "value"
+
+
+            // Write to the CSV where one column is the "name" and the other column is the "value"
+
+
+            // Increment to write to the next line
+
+
+    };
+
+    function DownloadCSV(filename) {
+
+    };
+
+});
+
