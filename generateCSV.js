8d13d7b014b18cd01f3a9f276c6f32c92af00e02
Stephen Tan <82525229+stephen-tan@users.noreply.github.com>
2021-07-21 14:02:24 -0700

    Add files via upload
    added jquery for gathering values from select and input options

--- generateCSV.js
+++ generateCSV.js
@@ -1,4 +1,61 @@
-$(document).ready(function() {
+//Wait to run your initialization code until the DOM is fully loaded. This is needed
+// when wanting to access elements that are later in the HTML than the <script>.
+// if(document.readyState === 'loading') {
+//     document.addEventListener('DOMContentLoaded', afterLoaded);
+// } else {
+//     //The DOMContentLoaded event has already fired. Just run the code.
+//     afterLoaded();
+// };
+
+// function afterLoaded() {
+//     //Your initialization code goes here. This is from where your code should start
+//     //  running if it wants to access elements placed in the DOM by your HTML files.
+//     //  If you are wanting to access DOM elements inserted by JavaScript, you may need
+//     //  to delay more, or use a MutationObserver to see when they are inserted.
+//     var parameters = document.querySelectorAll(".list-group-item");
+//     console.log(parameters);
+
+//     // JavaSript regex to get the element name in the id attribute
+//     let re = /(?<=id=")(.*?)(?=")/
+
+//     for (var i=0; i<parameters.length; i++) {
+//         var variableName = parameters[i].innerHTML.match(re)[0];
+        
+//         console.log(parameters[i]);
+        
+//         console.log(variableName);
+
+//     };
+
+
+// };
+
+var form = $("#methodParameters");
+var showSelectedParameters = $("#showSelectedParameters");
+
+showSelectedParameters.on('click', function() {
+
+    console.log("button pressed!");
+    console.log("------------------------");
+
+    var parameters = document.querySelectorAll(".list-group-item");
+
+    // JavaSript regex to get the element name in the id attribute
+    let re = /(?<=id=")(.*?)(?=")/
+
+    for (var i=0; i<parameters.length; i++) {
+        var variableName = parameters[i].innerHTML.match(re)[0];
+
+        var searchParameter = "#" + variableName;
+
+        var variableValue = $(searchParameter).find(':selected').val();
+        var variableValue = $(searchParameter).find('input').val();
+        
+
+        console.log(`${variableName}: ${variableValue}`);
+    };
+});
+
 
     function WriteToCSV() {
         // https://stackoverflow.com/questions/17564103/using-javascript-to-download-file-as-a-csv-file
@@ -18,10 +75,4 @@ $(document).ready(function() {
 
 
     };
-
-    function DownloadCSV(filename) {
-
-    };
-
-});
 
